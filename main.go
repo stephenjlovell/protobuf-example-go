ee4adfbabf0430a7678d244c544aba8fe3159221
Steve Lovell <sjlovell34@gmail.com>
2021-03-02 16:47:21 -0500

    import generated pb

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	simplepb "github.com/stephenjlovell/protobuf-example-go/src/simple"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	sm := simplepb.SimpleMessage{
+		Id:         12345,
+		IsSimple:   true,
+		Name:       "my simple message",
+		SampleList: []int32{1, 1, 2, 3, 5},
+	}
+	fmt.Printf("%+v\n", sm)
+	fmt.Printf(sm.GetId())
+
 }
